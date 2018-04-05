5fdc2efb34abf4a31c8542a616bd20e4374da81f
nguyenlananh <loglamo@gmail.com>
2018-04-05 15:58:38 +0700

    change8

--- KnockSwitch.py
+++ KnockSwitch.py
@@ -5,12 +5,7 @@ import time
 colors = [0xFF00, 0x00FF, 0x0FF0, 0xF00F]
 pins = {'pin_R':15, 'pin_G':16}  # pins is a dict
 GPIO.setmode(GPIO.BOARD)       # Numbers GPIOs by physical location
-for i in pins:
-	GPIO.setup(pins[i], GPIO.OUT)   # Set pins' mode is output
-	GPIO.output(pins[i], GPIO.HIGH) # Set pins to high(+3.3V) to off led
 
-p_R = GPIO.PWM(pins['pin_R'], 5)  # tan so xung
-p_G = GPIO.PWM(pins['pin_G'], 5)
 
 def map(x, in_min, in_max, out_min, out_max):
 	return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
@@ -31,7 +26,7 @@ def setColor(col):   # For example : col = 0x112233
 
 
 KnockPin = 11 
-LedPin = 12
+
 
 #Led_status = 1
 
@@ -46,8 +41,14 @@ def swLed(ev=None):
 #	Led_status = not Led_status
 #	GPIO.output(LedPin, Led_status)  # switch led status(on-->off; off-->on)
     print "Having motion !!!!"
-    p_R.start(1)      # Initial duty Cycle = 0(leds on)
-    p_G.start(1)
+    for i in pins:
+	   GPIO.setup(pins[i], GPIO.OUT)   # Set pins' mode is output
+	   GPIO.output(pins[i], GPIO.HIGH) # Set pins to high(+3.3V) to off led
+
+    p_R = GPIO.PWM(pins['pin_R'], 5)  # tan so xung
+    p_G = GPIO.PWM(pins['pin_G'], 5)  
+    p_R.start(0)      # Initial duty Cycle = 0(leds on)
+    p_G.start(0)
     for col in colors:
 	  setColor(col)
 	  time.sleep(2)
